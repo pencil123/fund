39336fdcc2f7e3b24e4c94fd81f399fc8857412d
张鲁燕 <zhangluyan@jsh.com>
2021-12-24 19:46:14 +0800

    init

---
+++ src/test/java/cn/blogscn/fund/FundApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.blogscn.fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
