456dd9e062833c9918097ab71fa520a0c22623da
张鲁燕 <zhangluyan@jsh.com>
2021-12-27 21:32:41 +0800

    更新AVG均值

--- src/main/java/cn/blogscn/fund/service/FundService.java
+++ src/main/java/cn/blogscn/fund/service/FundService.java
@@ -4,4 +4,7 @@ import cn.blogscn.fund.model.domain.Fund;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface FundService extends IService<Fund> {
+    Boolean updateAvgWeek(String fundCode);
+    Boolean updateAvgMonth(String fundCode);
+    Boolean updateAvg3month(String fundCode);
 }
--- src/test/java/cn/blogscn/fund/FundApplicationTests.java
+++ src/test/java/cn/blogscn/fund/FundApplicationTests.java
@@ -1,6 +1,8 @@
 package cn.blogscn.fund;
 
+import cn.blogscn.fund.service.FundService;
 import cn.blogscn.fund.service.job.SyncData;
+import com.baomidou.mybatisplus.annotation.TableName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 class FundApplicationTests {
     @Autowired
     private SyncData syncData;
+    @Autowired
+    private FundService fundService;
 
     @Test
     void test() {
         syncData.syncFundData();
+    }
+
+    @Test
+    void updateAvgWeek(){
+        fundService.updateAvgWeek("519983");
+        fundService.updateAvgMonth("519983");
+        fundService.updateAvg3month("519983");
     }
 
 }
